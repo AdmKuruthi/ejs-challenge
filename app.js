282dd4b4263311e30732b6aca5ff9fe5add615b9
acalderonc <acalderonc@ucenfotec.ac.cr>
2022-03-15 23:29:03 -0600

    Finished module

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _string = require('lodash/string');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,11 +16,56 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.locals._string = _string;
 
 
+//Arrays
+const posts = [];
 
 
+/*Router */
+app.get('/', (req,res) => {
+  res.render("home", {homeStartingContent: homeStartingContent, posts: posts});
+});
 
+app.get('/about', (req,res) => {
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get('/contact', (req,res) => {
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get('/compose', (req,res) => {
+  res.render("compose");
+});
+
+app.post('/compose', (req,res) => {
+  const postData = {
+    postTitle : req.body.composeTitle,
+    postText : req.body.composePost
+  }
+  
+  posts.push(postData);
+  res.redirect('/');
+});
+
+app.get("/posts/:postTitle", (req, res) => {
+  const title = req.params.postTitle;
+  const post = posts.filter((o) =>{
+      if(_string.kebabCase(o.postTitle.toLowerCase()) == title)
+        return o;
+  });
+  if(post.length > 0)
+    res.render("post", {post: post[0]});
+  else{
+    const notFound = {
+      postTitle: "Title not found",
+      postText: "The post you're looking for doesn't exist"
+    };
+    res.render("post", {post: notFound});
+  }
+});
 
 
 
